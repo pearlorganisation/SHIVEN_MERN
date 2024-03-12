b10f282a20713d0e57e28fbd29eab543f532edcd
Saksham <saksham@pearlorganisation.com>
2024-03-12 18:36:22 +0530

    added the global error handling

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,10 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { developmentWhiteListedIpAddresses, productionWhiteListedIpAddresses } from "./src/Utils/index.js";
+import {
+  developmentWhiteListedIpAddresses,
+  productionWhiteListedIpAddresses,
+} from "./src/Utils/index.js";
 // -------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------
 dotenv.config();
@@ -30,6 +33,30 @@ app.use(
 
 // -------------------------------------------------------------------------------------------------------------
 // -------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------Routes----------------------------------------------------
+app.all(["/", "/api", "/api/v1"], (req, res, next) => {
+  return res.status(200).json({
+    message: "Welcome to Shiven",
+  });
+});
+// -------------------------------------------------------------------------------------------------------------
+
+// ------------------------------------------Global Error Handling----------------------------------------------
+app.all("*", (req, res, next) => {
+  const err = new Error(`No such ${req.originalUrl} url exists`);
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((error, req, res, next) => {
+  error.statusCode = error.statusCode || 500;
+  return res.status(error.statusCode).json({
+    success: false,
+    message: error.message,
+  });
+});
+// -------------------------------------------------------------------------------------------------------------
+
 app.listen(PORT, () => {
   console.log(`Server Running at port ${PORT}`);
 });

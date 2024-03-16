88e8d2f6bf55ce861080349a9e539bbd0f55c4fd
Saksham <saksham@pearlorganisation.com>
2024-03-16 12:58:37 +0530

    added the user routes

--- backend/src/Routes/Auth/User/userRoutes.js
+++ backend/src/Routes/Auth/User/userRoutes.js
@@ -10,3 +10,12 @@ userRouter.route("/").post();
 
 // getUsers
 userRouter.route("/").get();
+
+// updateUser
+userRouter.route("/:userId").patch();
+
+// deleteUser
+userRouter.route("/:userId").delete();
+
+// getIndividualUser
+userRouter.route("/:userId").get();

817ce744b93f6c789388dbfa4dd259b6eaaeddd8
Saksham <saksham@pearlorganisation.com>
2024-03-19 15:28:10 +0530

    created the update user controller

--- backend/src/Controllers/Auth/User/userController.js
+++ backend/src/Controllers/Auth/User/userController.js
@@ -1,23 +1,32 @@
 // -----------------------------------------------Imports------------------------------------------------
-import { userModel } from "../../../Models/Auth/User/userModel";
-import { CustomError } from "../../../Utils/Error/CustomError";
-import { asyncErrorHandler } from "../../../Utils/Error/asyncErrorHandler";
+import { userModel } from "../../../Models/Auth/User/userModel.js";
+import { CustomError } from "../../../Utils/Error/CustomError.js";
+import { asyncErrorHandler } from "../../../Utils/Error/asyncErrorHandler.js";
 import { pick } from "lodash-es";
+import mongoose from "mongoose";
+// ------------------------------------------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------------------------------
+const { ObjectId } = mongoose.Types;
 // ------------------------------------------------------------------------------------------------------
 
 // @desc - createUser
 //@method- POST
-// @url - /user
-
+// @url - auth/user
 export const createUser = asyncErrorHandler(async (req, res, next) => {
-  const { name, email, password } = req?.body?.payload;
+  const { name, email, password, role } = req?.body?.payload;
 
   if (!name || !email || !password || !role) {
     const error = new CustomError("Please Fill Complete Details", 400);
     return next(error);
   }
 
-  const payload = pick(req?.body?.payload, ["name", "email", "password"]);
+  const payload = pick(req?.body?.payload, [
+    "name",
+    "email",
+    "password",
+    "role",
+  ]);
 
   const userDoc = new userModel(payload);
 
@@ -26,5 +35,49 @@ export const createUser = asyncErrorHandler(async (req, res, next) => {
   return res.status(200).json({
     success: true,
     message: "User Created Successfully",
+  });
+});
+
+// @desc - getUser
+// @method- GET
+// @url - auth/user
+export const getUser = asyncErrorHandler(async (req, res, next) => {
+  const users = await userModel.find();
+
+  return res.status(200).json({
+    success: true,
+    message: "Users Found Successfully",
+    users,
+  });
+});
+
+// @desc - updateUser
+// @method - PATCH
+// @url - auth/user/:userId
+export const updateUser = asyncErrorHandler(async (req, res, next) => {
+  const { userId } = req?.params;
+
+  if (!userId) {
+    const error = new CustomError("User Id must be provided", 400);
+    return next(error);
+  }
+
+  const payload = req?.body?.payload || {};
+
+  const user = await userModel.findOneAndUpdate(
+    { _id: userId },
+    { $set: payload },
+    { new: true }
+  );
+
+  if (!user) {
+    const error = new CustomError("Invalid User Id", 400);
+    return next(error);
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "User Found Successfully",
+    user,
   });
 });
--- backend/src/Routes/Auth/User/userRoutes.js
+++ backend/src/Routes/Auth/User/userRoutes.js
@@ -1,18 +1,23 @@
 // ---------------------------------------------Imports--------------------------------------------------
 import { Router } from "express";
+import {
+  createUser,
+  getUser,
+  updateUser,
+} from "../../../Controllers/Auth/User/userController.js";
 // ------------------------------------------------------------------------------------------------------
 
 export const userRouter = Router();
 // -------------------------------------------------------------------------------------------------------
 
 // createUser
-userRouter.route("/").post();
+userRouter.route("/").post(createUser);
 
 // getUsers
-userRouter.route("/").get();
+userRouter.route("/").get(getUser);
 
 // updateUser
-userRouter.route("/:userId").patch();
+userRouter.route("/:userId").patch(updateUser);
 
 // deleteUser
 userRouter.route("/:userId").delete();

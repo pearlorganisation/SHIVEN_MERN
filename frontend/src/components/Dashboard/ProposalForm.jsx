caa26afad98f0e00260362b824e6fcff623a7392
Rahul Rawat <rahulrawat@pearlorganisation.com>
2024-04-25 11:01:41 +0530

    changes

--- frontend/src/components/Dashboard/ProposalForm.jsx
+++ frontend/src/components/Dashboard/ProposalForm.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const ProposalForm = () => {
   const [showinput, setShowinput] = useState(false);
   const handelChange = (e) => {
     setShowinput(e.target.value == "yes");
   };
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+
+  console.log(watch("example")) 
   return (
     <>
       <div className="container mx-auto p-5 ">
@@ -22,7 +34,9 @@ const ProposalForm = () => {
                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                 placeholder=" "
                 required
+               
               />
+              
               <label
                 for="floating_first_name"
                 class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
@@ -355,6 +369,7 @@ const ProposalForm = () => {
           </div>
 
           <div class="grid md:grid-cols-2 md:gap-6">
+            
             <div class="relative z-0 w-full mb-5 group">
               <input
                 type="text"

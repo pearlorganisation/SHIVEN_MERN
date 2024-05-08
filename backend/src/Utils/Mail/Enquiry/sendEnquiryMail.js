0499d0b8140975b29f313db66e823d13f689fb65
Saksham <saksham@pearlorganisation.com>
2024-05-08 18:12:55 +0530

    configured the nodemailer for enquiry

--- backend/src/Utils/Mail/Enquiry/sendEnquiryMail.js
+++ backend/src/Utils/Mail/Enquiry/sendEnquiryMail.js
@@ -1,12 +1,32 @@
 // ----------------------------------------------Imports-------------------------------------------------------
-import nodemailer from "nodemailer";
+import nodemailer, { createTransport } from "nodemailer";
 // ------------------------------------------------------------------------------------------------------------
 
+export const sendEnquiryMail = (email) => {
+  const transporter = createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    service: "gmail",
+    auth: {
+      user: process.env.NODEMAILER_MAIL,
+      pass: process.env.NODEMAILER_MAIL_PASSWORD,
+    },
+  });
 
-export const sendEnquiryMail = ()=>{
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+  let mailOptions = {
+    from: process.env.NODEMAILER_MAIL,
+    to: email,
+    subject: "Consultation Enquiry",
+    html: "Enquiry Content",
+  };
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return reject(error);
+      } else {
+        return resolve("Mail Sent Successfully");
+      }
+    });
+  });
+};

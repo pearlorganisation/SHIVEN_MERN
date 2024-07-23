b9533850c2da38189c3fbf9528e3cc82077fc9ea
Avnish Negi <avnish@pearlorganisation.com>
2024-07-23 12:32:53 +0530

    live

--- frontend/src/features/actions/Auth/authActions.js
+++ frontend/src/features/actions/Auth/authActions.js
@@ -36,9 +36,7 @@ export const logout = createAsyncThunk(
   "auth/logout",
   async (payload, { rejectWithValue }) => {
     try {
-      const response = await instance.post("/auth/logout",{}, {
-        withCredentials: true,
-      });
+      const response = await instance.post("/auth/logout");
     } catch (error) {
       return rejectWithValue(error);
     }
